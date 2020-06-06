02a475ffe97d0dcf33e560b121d7c20f0fd58053
Victor Lopez <vmlopez.r@gmail.com>
2020-06-06 15:03:00 -0500

    Updates

---
+++ precache-manifest.9a6ea043fbcac7f25b0f86d6fc757d41.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd146bf24aa8eb7c416d57c6ae34a301",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "615936cade267c0d2cef",
+    "url": "/Portfolio/static/css/main.e848e946.chunk.css"
+  },
+  {
+    "revision": "861b340e831c27a2a2ec",
+    "url": "/Portfolio/static/js/2.e5e1945f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.e5e1945f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615936cade267c0d2cef",
+    "url": "/Portfolio/static/js/main.9d420ce7.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  },
+  {
+    "revision": "3147bf6ec9ff91a3d330387e2f4c57a3",
+    "url": "/Portfolio/static/media/modular-led.3147bf6e.mp4"
+  },
+  {
+    "revision": "cbb9940faa676c60bfca076befa160b5",
+    "url": "/Portfolio/static/media/modular-led.cbb9940f.png"
+  },
+  {
+    "revision": "27ca131681ce75c8ccd34d328694668c",
+    "url": "/Portfolio/static/media/owi-arm-model.27ca1316.mp4"
+  },
+  {
+    "revision": "6f746436ecc3a925de6479a586d1a2b3",
+    "url": "/Portfolio/static/media/owi-arm-model.6f746436.png"
+  },
+  {
+    "revision": "558e464d917a32528214402a072eed80",
+    "url": "/Portfolio/static/media/owi-arm-sorting.558e464d.png"
+  },
+  {
+    "revision": "8edf862a8b1243d2474167034e0b3ec2",
+    "url": "/Portfolio/static/media/robotics-project.8edf862a.mp4"
+  },
+  {
+    "revision": "13b64b86dab46544f60c0310afed6d3d",
+    "url": "/Portfolio/static/media/rpi-dashcam.13b64b86.mp4"
+  },
+  {
+    "revision": "519914eb86f30afd8b2173d4db84a66e",
+    "url": "/Portfolio/static/media/rpi-dashcam.519914eb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ca3d4907703fedbfb32c801da9e1abf.js"
+  "/Portfolio/precache-manifest.9a6ea043fbcac7f25b0f86d6fc757d41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
