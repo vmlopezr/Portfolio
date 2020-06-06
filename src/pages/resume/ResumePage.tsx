786e3f39fdcac5b5d17cbdb1acf9e05c56369746
Victor Lopez <vmlopez.r@gmail.com>
2020-06-06 15:02:02 -0500

    Added docker file for testing of source files with nginx. Updated content on the ResumePage as well as minor styling

--- src/pages/resume/ResumePage.tsx
+++ src/pages/resume/ResumePage.tsx
@@ -14,30 +14,31 @@ const Experience: position[] = [
     location: 'Tulsa, Oklahoma',
     company: 'Summit ESP, A Halliburton Company',
     bullets: [
-      'Redesigned the existing LabVIEW Product test to implement ModbusTCP communications with the new company instrument and record test data to SQLite databases. (LabView)',
-      'Implemented existing LabVIEW automatic test as a  kivy python application for test and validation of up to 32 pressure' +
-        'transducers through various pressure and temperature setpoints. Reduced test completion' +
-        'time by 90% down to 1 hour. (Python)',
-      'Constructed a batch script to configure ethernet routers via SSH protocol to enable ethernet' +
+      'Developed a kivy python front-end application for test, validation and logging of up to 32 pressure ' +
+        'transducers reducing test completion time by 90% down to 1 hour. (Python, SQLite)',
+      'Created a a raspberry pi modbus server to store live pressure transducer data retrieved via SPI peripheral. (Python)',
+      'Constructed a batch script that configures ethernet routers via SSH protocol to establish ethernet ' +
         'communications in multi-drive well-sites. (Batch Scriping, Teraterm)',
-      'Built ladder logic on an Idec FC6A+ PLC to enable ModbusTCP in Summit Products for  use in  ethernet networks.',
+      'Built ladder logic on an Idec FC6A+ PLC to enable ModbusTCP in Summit Products for use in ethernet networks. (Ladder Logic)',
+      'Redesigned the existing LabVIEW Product test to replace serial communications with ModbusTCP. Thus allowing ' +
+        'testing of the new company instrument and data logging to SQLite databases. (LabView, SQLite)',
     ],
   },
   {
     title: 'Electrical Engineering Co-op',
     dates: 'May 2019 - August 2019',
     location: 'Houston, Texas',
-    company: 'Lyondellbasell',
+    company: 'Lyondellbasell, Houston Refinery',
     bullets: [
+      'Developed excel dashboard depicting the site electrical subs system, for capital and long term planning.',
       'Discovered two critical 12KV motors are susceptible to insulation failure by identifying increasing negative polarity trend in Partial Discharge monitoring data.',
-      'Verified electrical feasibility for installation of vibration monitor on a Propylene-Propane pump. Redesigned control circuit to trip pump on high vibration setpoint and finalized project package to issue for approval.',
     ],
   },
   {
     title: 'Instrumentation & Electrical Engineering Co-op',
     dates: 'May 2018 - August 2018',
     location: 'Clinton, Iowa',
-    company: 'Lyondellbasell',
+    company: 'Lyondellbasell, Clinton Complex',
     bullets: [
       'Identified relieving air regulators in condensate pot service as failure points during low ambient temperatures in winter. Established project to replace relieving regulators in similar service with non-relieving versions.',
       'Discovered 38 pressure instruments on the Plant Deluge System have no preventative maintenance. Partnered with local fire safety service company to establish function testing procedure to include in the semiannual testing. ',
@@ -47,7 +48,7 @@ const Experience: position[] = [
     title: 'Instrumentation & Electrical Engineering Co-op',
     dates: 'Jan 2017 - August 2017',
     location: 'Pasadena, Texas',
-    company: 'Lyondellbasell',
+    company: 'Lyondellbasell: Bayport Polymers',
     bullets: [
       'Identified critical interlocks and designed functional testing procedures to validate the Safety Instrumented System of the plant. ',
       'Collaborated with engineers, operations and vendors in order to certify instrument specifications, set up spares for essential instruments, and updated the Bill of Materials for the associated units.',
