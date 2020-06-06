fad627326fa5597201878a5735cc4c3cf81989d2
Victor Lopez <vmlopez.r@gmail.com>
2020-06-06 11:32:09 -0500

    Updates

---
+++ precache-manifest.2ca3d4907703fedbfb32c801da9e1abf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6c1542fabf43ef22fc8f1831f1bac5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e23742f84069a532f6d3",
+    "url": "/static/css/main.ebdad328.chunk.css"
+  },
+  {
+    "revision": "861b340e831c27a2a2ec",
+    "url": "/static/js/2.e5e1945f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5e1945f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23742f84069a532f6d3",
+    "url": "/static/js/main.e6f7673a.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "3147bf6ec9ff91a3d330387e2f4c57a3",
+    "url": "/static/media/modular-led.3147bf6e.mp4"
+  },
+  {
+    "revision": "cbb9940faa676c60bfca076befa160b5",
+    "url": "/static/media/modular-led.cbb9940f.png"
+  },
+  {
+    "revision": "27ca131681ce75c8ccd34d328694668c",
+    "url": "/static/media/owi-arm-model.27ca1316.mp4"
+  },
+  {
+    "revision": "6f746436ecc3a925de6479a586d1a2b3",
+    "url": "/static/media/owi-arm-model.6f746436.png"
+  },
+  {
+    "revision": "558e464d917a32528214402a072eed80",
+    "url": "/static/media/owi-arm-sorting.558e464d.png"
+  },
+  {
+    "revision": "8edf862a8b1243d2474167034e0b3ec2",
+    "url": "/static/media/robotics-project.8edf862a.mp4"
+  },
+  {
+    "revision": "13b64b86dab46544f60c0310afed6d3d",
+    "url": "/static/media/rpi-dashcam.13b64b86.mp4"
+  },
+  {
+    "revision": "519914eb86f30afd8b2173d4db84a66e",
+    "url": "/static/media/rpi-dashcam.519914eb.png"
+  }
+]);
